9271decf0635248de75dffbc87fa45a635a40fa0
Bjorn Wickstrom <bjorn@billo.systems>
2017-10-31 10:29:48 -0500

    Added test profile.

--- project.clj
+++ project.clj
@@ -11,4 +11,11 @@
   :profiles {
     :dev {
       :dependencies [
-        [environ "0.4.0"]]}})
+        [environ "0.4.0"]]}
+    :test {
+      :plugins [
+        [lein-ancient "0.6.14"]
+        [jonase/eastwood "0.2.5"]
+        [lein-bikeshed "0.5.0" :exclusions [org.clojure/tools.namespace]]
+        [lein-kibit "0.1.5"]
+        [venantius/yagni "0.1.4"]]}})
