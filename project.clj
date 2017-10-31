63cbae1820e96dd5624dd79c2138a448e641b45f
Bjorn Wickstrom <bjorn@billo.systems>
2017-10-31 10:37:41 -0500

    Updated deps.

--- project.clj
+++ project.clj
@@ -5,13 +5,14 @@
     :name "Eclipse Public License"
     :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-    [org.clojure/clojure "1.5.1"]
-    [org.clojure/data.xml "0.0.7"]
-    [clj-http "0.7.2"]]
+    [org.clojure/clojure "1.8.0"]
+    [org.clojure/data.xml "0.0.8"]
+    [clj-http "3.7.0"]]
   :profiles {
     :dev {
       :dependencies [
-        [environ "0.4.0"]]}
+        [environ "1.1.0"]
+        [org.clojure/tools.namespace "0.2.11"]]}
     :test {
       :plugins [
         [lein-ancient "0.6.14"]
