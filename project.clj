02133a14fbbd908a65de287808ed0cdb29aec1db
Bjorn Wickstrom <bjorn@billo.systems>
2017-10-31 10:02:45 -0500

    New artifact id and dev version.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject usps-clj "0.1.1"
-  :description "Clojure interface to USPS's outdated API."
-  :url "https://github.com/banzai-inc/usps-clj"
+(defproject systems.billo/clj-usps "0.2.0-SNAPSHOT"
+  :description "Clojure interface to the USPS Web Tools API"
+  :url "https://github.com/billosys/clj-usps"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
