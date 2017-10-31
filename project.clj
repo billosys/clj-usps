0fcf8ab777c0e59d7d1fc93b85078227bc6da316
Bjorn Wickstrom <bjorn@billo.systems>
2017-10-31 10:04:19 -0500

    Reformatting.

--- project.clj
+++ project.clj
@@ -1,9 +1,14 @@
 (defproject systems.billo/clj-usps "0.2.0-SNAPSHOT"
   :description "Clojure interface to the USPS Web Tools API"
   :url "https://github.com/billosys/clj-usps"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/data.xml "0.0.7"]
-                 [clj-http "0.7.2"]]
-  :profiles {:dev {:dependencies [[environ "0.4.0"]]}})
+  :license {
+    :name "Eclipse Public License"
+    :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [
+    [org.clojure/clojure "1.5.1"]
+    [org.clojure/data.xml "0.0.7"]
+    [clj-http "0.7.2"]]
+  :profiles {
+    :dev {
+      :dependencies [
+        [environ "0.4.0"]]}})
