165ab808d34f3d6e7abda77729ec6f6bf99f814d
Bjorn Wickstrom <bjorn@billo.systems>
2017-10-31 10:29:19 -0500

    Bump version for next dev cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject systems.billo/clj-usps "0.2.0"
+(defproject systems.billo/clj-usps "0.3.0-SNAPSHOT"
   :description "Clojure interface to the USPS Web Tools API"
   :url "https://github.com/billosys/clj-usps"
   :license {
