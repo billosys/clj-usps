4d86879ea92c2e477488c7591d50267b7f19688f
Bjorn Wickstrom <bjorn@billo.systems>
2017-11-01 21:39:45 -0500

    Moved system tests into own ns.

--- test/usps/tests/system/address.clj
+++ test/usps/tests/system/address.clj
@@ -1,20 +1,23 @@
-(ns ^:system usps.address-test
-  (:require [clojure.test :refer :all]
-            [environ.core :refer :all]
-            [usps.address :as address]))
+(ns ^:system usps.tests.system.address
+  (:require
+    [clojure.test :refer :all]
+    [environ.core :refer :all]
+    [usps.address :as address]))
 
 (deftest addresses
   (let [usps-api-url (env :usps-api-url)
         usps-user-id (env :usps-user-id)]
 
     (testing "verify address"
-      (is (= {:street "6406 IVY LN" :city "GREENBELT" :state "MD" :zip "20770" :zip_four "1441"}
+      (is (= {:street "6406 IVY LN" :city "GREENBELT" :state "MD" :zip "20770"
+              :zip_four "1441"}
              (address/validate
                {:street "6406 ivy lane" :city "Greenbelt" :state "MD" :zip ""}
                usps-api-url usps-user-id))))
 
     (testing "remove bad characters"
-      (is (= {:street "6406 IVY LN" :city "GREENBELT" :state "MD" :zip "20770" :zip_four "1441"}
+      (is (= {:street "6406 IVY LN" :city "GREENBELT" :state "MD" :zip "20770"
+              :zip_four "1441"}
              (address/validate
                {:street "6406 IVY & LANE" :city "Greenbelt" :state "MD" :zip ""}
                usps-api-url usps-user-id))))
